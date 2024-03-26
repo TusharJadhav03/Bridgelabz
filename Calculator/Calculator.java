d7cf61abe6924adb367d298553f125fcfb31c496
Tushar Jadhav <tushar.jadhav.work@gmail.com>
2024-03-26 11:29:28 +0530

    file add

---
+++ Calculator/Calculator.java
@@ -0,0 +1,20 @@
+package Calculator;
+
+import java.util.Scanner;
+
+/**
+ * Calculator
+ */
+public class Calculator {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number :");
+        int x = sc.nextInt();
+        System.out.println("Enter the second number :");
+        int y = sc.nextInt();
+
+        
+    }
+}
